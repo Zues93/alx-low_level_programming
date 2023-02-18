d8a9f05cfe1e6e7787a7d505791c8b6596369c74
Abdessamad <abdessamadmahmoudi93@gmail.com>
2023-02-17 23:34:10 -0800

    positive

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -1,14 +1,24 @@
-int main(void)
-{
-char ch;
+#include <stdio.h>
 
-for (ch = 'a'; ch <= 'z'; ch++)
-putchar(ch);
-
-for (ch = 'A'; ch <= 'Z'; ch++)
-putchar(ch);
-
+int main() {
+char c = 'a';
+int i;
+    
+// Print lowercase letters
+for (i = 0; i < 26; i++) {
+putchar(c);
+c++;
+}
+    
+c = 'A';
+    
+// Print uppercase letters
+for (i = 0; i < 26; i++) {
+putchar(c);
+c++;
+}
+    
 putchar('\n');
+    
 return 0;
 }
-
