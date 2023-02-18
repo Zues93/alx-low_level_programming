0c9ee70f0532af755342c1bf54e622bd85ef6d3c
Abdessamad <abdessamadmahmoudi93@gmail.com>
2023-02-18 11:30:43 -0800

    Inventing

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -1,11 +1,18 @@
 #include <stdio.h>
 
+/**
+ * main - Entry point
+ *
+ * This function prints all possible combinations of two two-digit numbers.
+ *
+ * Return: Always 0
+ */
 int main(void)
 {
 int i;
 int j;
 
-i = 0
+i = 0;
 while (i <= 8)
 {
 j = i + 1;
@@ -15,8 +22,8 @@ putchar(i + 48);
 putchar(j + 48);
 if (i < 8 || j < 9)
 {
-putchar(',')
-putchar(' ')
+putchar(',');
+putchar(' ');
 }
 j++;
 }
