2e9dd8e4b7102675fdaf6e9a3f7c65178cbc34c3
Abdessamad <abdessamadmahmoudi93@gmail.com>
2023-02-18 07:45:20 -0800

    alphabet soup

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - main block
+ * Description: get a random number and check its 
+ * Ruturn: 0 
+ */
+int main(void)
+{
+char c = 'a';
+while (c <= '2')
+{
+if (c != 'e' && c != 'q')
+{
+putchar(c);
+}
+c++;
+}
+putchar('\n');
+return (0);
+}
