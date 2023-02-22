9169496e54e05d4e6c99e10065f8af17ea2ad4f7
Abdessamad <abdessamadmahmoudi93@gmail.com>
2023-02-22 05:44:06 -0800

    lowercase

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -2,14 +2,14 @@
 /**
  * that prints the alphabet, in lowercase
  */
-void print_alphabet(void);
+void print_alphabet(void)
 {
-	char c;
-	c = 'a';
-	while (c <= 'z')
-	{
-		_putchar(c);
-		c++;
-	}
-	 _putchar('\n');
+char c;
+c = 'a';
+while (c <= 'z')
+{
+_putchar(c);
+c++;
+}
+_putchar('\n');
 }
