ec498c68828a022b8229bcc51482d075233431e8
Abdessamad <abdessamadmahmoudi93@gmail.com>
2023-03-02 23:37:03 -0800

    stranca

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -1,14 +1,14 @@
 #include "main.h"
 /**
- * _strncat - concatenate two strings
- * using at most n bytes from src
- * @dest: input value
- * @src: input value
- * @n: input value
- *
- * Return: dest
- */
-char *_strncpy(char *dest, char *src, int n);
+* _strncat - concatenate two strings
+* using at most n bytes from src
+* @dest: input value
+* @src: input value
+* @n: input value
+*
+* Return: dest
+*/
+char *_strncat(char *dest, char *src, int n)
 {
 int i;
 int j;
--- 0x06-pointers_arrays_strings/README.md
+++ 0x06-pointers_arrays_strings/README.md
@@ -1,1 +1,1 @@
- - More pointers, arrays and strings
+C - More pointers, arrays and strings
