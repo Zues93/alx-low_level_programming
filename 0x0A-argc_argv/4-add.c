8850d3923e09d2ea247ffb218db3c11ac2c292a1
Abdessamad <abdessamadmahmoudi93@gmail.com>
2023-03-10 14:32:58 -0800

    add

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -27,10 +27,10 @@ return (1);
  * main - Print the name of the program
  * @argc: Count arguments
  * @argv: Arguments
- * 
+ *
  * Return: Always 0 (Success)
  */
- int main(int argc, char *argv[])
+int main(int argc, char *argv[])
 {
 /*Declaring variables*/
 int count;
